c97e30ff436933a0a5390e60b759085238492a21
ArmanDesir <froschcrow@gmail.com>
2025-05-08 05:39:39 +0800

    add full Flutter project

---
+++ android/app/src/main/kotlin/com/example/casptone_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.casptone_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/veiws/student/lessons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StudentLessons extends StatelessWidget {
+  const StudentLessons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Lessons Screen', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
